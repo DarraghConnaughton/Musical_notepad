86a06fd9335380291a8c4c1667b4e0c5cc9761cb
Darragh Connaughton <Darragh.connaughton5@mail.dcu.ie>
2017-04-18 23:15:41 +0100

    Popup Menu added.

--- app/src/main/assets/js/displayMusic.js
+++ app/src/main/assets/js/displayMusic.js
@@ -1,9 +1,12 @@
 function displayMusic(){
-//        var chorus =  "%%staffwidth 200\nX: 1 \nT: Cooley's \nM: 4/4 \nL: 1/8 \nK: Emin \n|:D2|EmEB{c}BA B2 EB|~B2 AB dBAG|\nDFDAD BDAD|FDAD dAFD| \nEmEBBA B2 EB:|";
         var chorus = getTuneDetails();
         ABCJS.renderAbc('notation', chorus);
 }
 
 function getTuneDetails(){
     return Android.sendData();
+}
+
+function chordProgression(){
+    return Android.chordProgression();
 }
