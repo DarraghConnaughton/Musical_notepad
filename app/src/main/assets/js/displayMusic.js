1e442648d80ab7557dd6ae295c2a306267d6aac1
Darragh Connaughton <Darragh.connaughton5@mail.dcu.ie>
2017-05-08 19:30:47 +0100

    MIDI playback implemented.

--- app/src/main/assets/js/displayMusic.js
+++ app/src/main/assets/js/displayMusic.js
@@ -7,6 +7,11 @@ function getTuneDetails(){
     return Android.sendData();
 }
 
+function playAudio(){
+    var piano = new Instrument('piano');
+    piano.play(chordProgression());
+}
+
 function chordProgression(){
     return Android.chordProgression();
 }
